c8e366b6455885903c260562804635246c623f81
Avinash B <nektworks@outlook.com>
2019-11-25 20:48:49 +0100

    Added VSCode config.

---
+++ lncode.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+
+# Currently, I can only confirm that this works on Fedora 31.
+
+ln -s $HOME/dotfiles/vscode/settings.json $HOME/.config/Code/User/settings.json
+ln -s $HOME/dotfiles/vscode/keybindings.json $HOME/.config/Code/User/keybindings.json
+ln -s $HOME/dotfiles/vscode/snippets/ $HOME/.config/Code/User
